8afd17c7ec8c3a48ebb865c3a68303dfe4681e68
losevanni <kma1202smdm@gmail.com>
2023-05-26 12:43:28 +0900

    수정

--- chproxy.ps1
+++ chproxy.ps1
@@ -1,13 +1,18 @@
-function proxyOnOff {
+$IP='127.0.0.1'
+$PORT='8888'
+
+function proxyOnOff { # 꺼저있으면 키고 켜저있으면 끈다 동시에 결과를 알림창에 표시 
     $value=Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' | Select-Object ProxyEnable
     if ($value.ProxyEnable -eq 0) { # off 라면 키기
+        Write-Host "${IP}:${PORT} proxy on"
         Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 1
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "http://127.0.0.1:8888"
-        # New-BurntToastNotification -Text "ProxyOnOff",'proxy set On'
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "http://${IP}:${PORT}"
+        New-BurntToastNotification -Text "ProxyOnOff",'proxy set On'
         return "proxy set On"
     }elseif ($value.ProxyEnable -eq 1) {
+        Write-Host "proxy set off"
         Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 0
-        # New-BurntToastNotification -Text "ProxyOnOff",'proxy set Off'
+        New-BurntToastNotification -Text "ProxyOnOff",'proxy set Off'
         return "proxy set Off"
     }
 }
--- shareFile.ps1
+++ shareFile.ps1
@@ -5,20 +5,18 @@ param ( # 실랭시 인수로 option 과 파일경로,명 넣기
 Write-Host "input Option: $Option& Path: $Path"
 # $OK=Read-Host "ok ? 아무키나 입력 하시오 key enter"
 # 만약 옵션이 없고 인자가 한게 경로라면 down을 기본으로 하여 파일다운함
-if($Path){ 
-    $Path=$Option
-    $Option='d' #기본값
-}
+# if($Path){ 
+#     $Path=$Option
+#     $Option='d' #기본값
+# }
 if($Option -ceq 'u' -or $Option -ceq '-u'){
     #업로드
-    $err=scp.exe $Path testserver_v:/home/vanni/shareFile
+    scp.exe -p $Path testserver_v:/home/vanni/shareFile
 }elseif ($Option -ceq 'd' -or $Option -ceq '-d') {
     #다운로드
-    $err=scp.exe testserver_v:/home/vanni/shareFile/$Path
+    scp.exe testserver_v:/home/vanni/shareFile/$Path .
 }else{
     Write-Host "fail please up or down"
 }
-Write-Host $err
-
 #다음버전 반복문으로 실패해도 다시 시작 하기와 여러 파일 업로드및 다운로드 하기 
 # 아마 배열을 사용해서 길이 만큼 처리하고 실패한 파일 명은 따로 모아 출력 하기
