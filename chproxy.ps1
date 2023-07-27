e7f69731e8f98e9281a65e61f6efc8a4a1301d41
losevanni <kms1202smdm@gmail.com>
2023-07-27 12:56:54 +0900

    good

--- chproxy.ps1
+++ chproxy.ps1
@@ -3,23 +3,11 @@ param ( # 실행시 인수로 option 과 파일경로,명 넣기
     [string]$Option
 )
 function proxyOnOff {
-    $IP='127.0.0.1'
-    $PORT='8888'
     $value=Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' | Select-Object ProxyEnable
     if ($value.ProxyEnable -eq 0) { # off 라면 키기
-        if($Option){
-            # ip 를 물어보는 로직 추가
-            Write-Host "IP 를 입력 하세요"
-            $IP=Read-Host
-            Write-Host "PORT 번호를 입력 하세요"
-            $PORT=Read-Host
-            if(!$PORT){
-                $PORT='8888'
-            }
-        }
-        Write-Host "${IP}:${PORT} Proxy On"
         Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 1
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "${IP}:${PORT}"
+        $ret=Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' | Select-Object ProxyServer
+        Write-Host "$ret Proxy On"
         New-BurntToastNotification -Text "ProxyOnOff",'proxy set On'
         return "proxy set On"
     }elseif ($value.ProxyEnable -eq 1) {
@@ -29,11 +17,34 @@ function proxyOnOff {
         return "proxy set Off"
     }
 }
-if($Option -eq 'i'){
+function setvalue{
+    if($Option){
+        # ip 를 물어보는 로직 추가
+        Write-Host "IP 를 입력 하세요"
+        $IP=Read-Host
+        if(!$IP){
+            $IP='127.0.0.1'
+        }
+        Write-Host "PORT 번호를 입력 하세요"
+        $PORT=Read-Host
+        if(!$PORT){
+            $PORT='8888'
+        }
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "${IP}:${PORT}"
+        Write-Host "${IP}:${PORT} 설정"
+    }
+}
+function catset {
     $SETINFO=Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" | Select-Object ProxyEnable
     $IPPORTINFO=Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" | Select-Object ProxyServer
     Write-Host "$SETINFO $IPPORTINFO 설정"
-}else{
-    proxyOnOff
 }
 
+if(!$Option){ # null 이면 on off
+    proxyOnOff
+}elseif($Option -eq 'i'){ # 설정 보여주기
+    catset
+}else{ # ip port 설정
+    setvalue
+}
+    
