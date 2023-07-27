5bc16d509f63cb37890fd94a0b043f3b3e6f230b
losevanni <kma1202smdm@gmail.com>
2023-07-27 23:56:07 +0900

    path compact

--- chproxy.ps1
+++ chproxy.ps1
@@ -1,19 +1,53 @@
-$IP='127.0.0.1'
-$PORT='8888'
+
+param ( # 실행시 인수로 option 과 파일경로,명 넣기
+    [string]$Option
+)
+$NETPATH='HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
 function proxyOnOff {
-    $value=Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' | Select-Object ProxyEnable
+    $value=Get-ItemProperty -Path $NETPATH | Select-Object ProxyEnable
     if ($value.ProxyEnable -eq 0) { # off 라면 키기
-        Write-Host "${IP}:${PORT} Proxy On"
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 1
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "http://${IP}:${PORT}"
-        New-BurntToastNotification -Text "ProxyOnOff",'proxy set On'
+        Set-ItemProperty -Path $NETPATH -Name ProxyEnable -Value 1
+        $ret=Get-ItemProperty -Path $NETPATH | Select-Object ProxyServer
+        Write-Host "$ret Proxy On"
+        New-BurntToastNotification -Text "ProxyOnOff",$ret' ON'
         return "proxy set On"
     }elseif ($value.ProxyEnable -eq 1) {
         Write-Host "proxy Off"
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 0
+        Set-ItemProperty -Path $NETPATH -Name ProxyEnable -Value 0
         New-BurntToastNotification -Text "ProxyOnOff",'proxy set Off'
         return "proxy set Off"
     }
 }
-proxyOnOff
+function setvalue{
+    if($Option){
+        Write-Host "IP 를 입력 하세요"
+        $IP=Read-Host
+        if(!$IP){
+            $IP='127.0.0.1'
+        }
+        Write-Host "PORT 번호를 입력 하세요"
+        $PORT=Read-Host
+        if(!$PORT){
+            $PORT='8888'
+        }
+        Set-ItemProperty -Path $NETPATH -Name ProxyServer -Value "${IP}:${PORT}"
+        New-BurntToastNotification -Text "Proxy Set IP, PORT",${IP}':'${PORT}' 설정 완료'
+        Write-Host "${IP}:${PORT} 설정"
+    }
+}
+function catinfo {
+    $SETINFO=Get-ItemProperty -Path $NETPATH | Select-Object ProxyEnable
+    $IPPORTINFO=Get-ItemProperty -Path $NETPATH | Select-Object ProxyServer
+    Write-Host "$SETINFO $IPPORTINFO 설정"
+}
 
+if(!$Option){ # null 이면 on off
+    proxyOnOff
+}elseif($Option -ceq 'i' -or $Option -ceq '-i'){ # 설정 보여주기
+    catinfo
+}else{ # i 외에 옵션은 전부 ip port 설정
+    setvalue
+}
+
+# next catset -> info 이름 변경 , 번트토스트에 ip port 번호 나올수있게 수정 하기
+    
